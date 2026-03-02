c12927d0ee3be6c4acd2de5afe954d7bb9c133ac
Deaguilon <deaguilon@carc.pshs.edu.ph>
2026-03-02 13:51:16 +0800

    Constructed program skeleton and basic menu system

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 def gradeManagement():
-    pass
+    print("[PLACEHOLDER - insert grade manager code]")
 
 def scheduler():
-    pass
+    print("[PLACEHOLDER - insert grade manager code]")
 
 def main():
     while True:
+        print()
         print("Menu:")
         print("1. Grade Management System")
         print("2. Schedule Tracker")
         print("3. Exit")
         choice = int(input("Enter the number corresponding to your choice: "))
+        print()
 
         match choice:
             case 1:
@@ -21,3 +23,5 @@ def main():
                 break
             case _:
                 print("Please enter a number between 1 and 3.")
+
+main()
