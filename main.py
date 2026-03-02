068036724a0f53159c28b4e502bc1f396f3d080a
Deaguilon <deaguilon@carc.pshs.edu.ph>
2026-03-02 14:06:59 +0800

    Built another menu (tayab)

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+import json
+
 def gradeManagement():
-    print("[PLACEHOLDER - insert grade manager code]")
+    while True:
+        print("1. Record grades")
+        print("2. Display grade summary")
+        print("3. Exit")
+        choice = int(input("Enter the number corresponding to your choice: "))
+
+        match choice:
+            case 1:
+                print("[PLACEHOLDER - insert code for recording grades to a file]")
+            case 2:
+                print("[PLACEHOLDER - insert code for displaying a summary of the grades]")
+            case 3:
+                break
+            case _:
+                print("Please enter a number between 1 and 3.")
+
 
 def scheduler():
     print("[PLACEHOLDER - insert grade manager code]")
