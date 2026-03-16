0f525d59fa3ed6f774ee2ad51714394830f5aff7
8-Camia <deaguilon@carc.pshs.edu.ph>
2026-03-16 14:02:26 +0800

    Implemented simple Display System for both deadlines and grade manager

--- main.py
+++ main.py
@@ -34,15 +34,9 @@ def gradeManagement():
         "maximum": 0.0
     }
 
-    gradeSum = {
-        "subject": "",
-        "units": 0.0,
-        "grade": 0.0,
-    }
-
     # Set target file
     activityFilename = "activities.json"
-    gradesFilename = ""
+
     # Load data from file, store it in memory as a variable
     with open(activityFilename, 'r') as loadedFile:
         activityData = json.load(loadedFile)
@@ -127,13 +121,14 @@ def gradeManagement():
                             print()
 
             case 2:
-                print("[PLACEHOLDER - insert code for displaying a summary of the grades]")
+                for activities in activityData:
+                    print(activities)
                 print()
-                int(input(""))
             case 3:
                 break
             case _:
                 print("Please enter a number between 1 and 3.")
+                print()
 
 # Function for the scheduler
 def scheduler():
@@ -157,7 +152,6 @@ def scheduler():
 
     while True:
         # Display menu, take user's choice
-        checkDeadlines(deadlineData, dateFormat)
         print("1. Record due dates")
         print("2. Display deadline summary")
         print("3. Back")
@@ -221,19 +215,9 @@ def scheduler():
                                 case _:
                                     print("Please enter either y(es) or n(o).")
                                     print()
-
-                    # Prompt user to record another activity (or not...)
-                    choice = input("Would you like to record another activity? (y/n) ").lower()
-                    match choice:
-                        case "y":
-                            pass
-                        case "n":
-                            break
-                        case _:
-                            print("Please enter either y(es) or n(o).")
-                            print()
             case 2:
-                print("[PLACEHOLDER - insert code for displaying a summary of the recorded deadlines]")
+                for deadlines in deadlineData:
+                    print(deadlines)
                 print()
             case 3:
                 break
@@ -241,20 +225,22 @@ def scheduler():
                 print("Please enter a number between 1 and 3.")
                 print()
 
+        # Prompt user to record another activity (or not...)
+        choice = input("Would you like to record another activity? (y/n) ").lower()
+        match choice:
+            case "y":
+                pass
+            case "n":
+                break
+            case _:
+                print("Please enter either y(es) or n(o).")
+                print()
+
 def printSubjectList():
     print("List of subjects:")
     # The enumerate() function allows you to grab the index of a list element alongside the element
     for index, subject in enumerate(subjectList):
         print(f"{index}. {subject}")
-
-def checkDeadlines(data, format):
-    almostDue = {}
-    for activity in data:
-        timeUntil = datetime.datetime.strptime(activity["deadline"], format) - datetime.datetime.now()
-        if timeUntil <= datetime.timedelta(days = 7):
-            almostDue[activity["name"]] = timeUntil
-    print("Note: the following activities are either almost due or overdue:")
-
 
 def instructions():
     print("Most, if not all, of this program's menus are navigated by entering a number based on your desired outcome.")
